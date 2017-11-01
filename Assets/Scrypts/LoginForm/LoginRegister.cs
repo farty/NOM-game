d6c31650b32dd649e56994293e897cef671feab5
VorbyT <artem.slepentsev@gmail.com>
2017-11-01 16:30:41 +0300

    Registration form added as new scene

---
+++ Assets/Scrypts/LoginForm/LoginRegister.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoginRegister : MonoBehaviour {
+
+    InputField _loginInput, _passwordInput;
+    private IEnumerator corutine;
+
+	// Use this for initialization
+	void Start () {
+        _loginInput = GameObject.Find("LoginInput").GetComponent<InputField>();
+        _passwordInput = GameObject.Find("PassInput").GetComponent<InputField>();
+        ResultTextBox.SetTextBox(GameObject.Find("Message").GetComponent<Text>());
+    }
+	
+    public void LoginOnClick()
+    {
+        corutine=WebAction("Login");
+        StartCoroutine(corutine);
+        Debug.Log("Login click!");
+    }
+
+    public void ResgisterOnClick()
+    {
+        corutine = WebAction("Register");
+        StartCoroutine(corutine);
+        Debug.Log("Register click!");
+    }
+
+    private IEnumerator WebAction(string action)
+    {
+        var url = UrlHelper.ConstructUrl(action, _loginInput.text, _passwordInput.text);
+        var request = new WWW(url);
+        _loginInput.text = "";
+        _passwordInput.text = "";
+        yield return request;
+        Debug.Log(request.text);
+        if(request.error != null)
+        {
+            ResultTextBox.SetResultMessage(request.error);
+        }
+        else
+        {
+            if(request.text.Trim() == "Correct")
+            {
+                ResultTextBox.SetResultMessage("Login Succesful!",true);
+                yield return new WaitForSeconds(5);
+                //TODO: Implement hook for main scene that passes username.
+                SceneManager.LoadScene(21);
+            }
+            if(request.text.Trim() == "Wrong"|| request.text.Trim() == "No User")
+            {
+                ResultTextBox.SetResultMessage("Incorrect login or password");
+                yield return new WaitForSeconds(5);
+                ResultTextBox.Clear();
+            }
+            if(request.text.Trim() == "ERROR")
+            {
+                ResultTextBox.SetResultMessage("Error accured, retarting...");
+                yield return new WaitForSeconds(5);
+                SceneManager.LoadScene(20);
+            }
+            if(request.text.Trim() == "ILLEGAL REQUEST")
+            {
+                ResultTextBox.SetResultMessage("Internal server error!");
+                yield return new WaitForSeconds(5);
+                ResultTextBox.Clear();
+            }
+            if(request.text.Trim()== "Registered")
+            {
+                //Should we log user in as soon as they register?
+                ResultTextBox.SetResultMessage("Registration Succesful! Now you can login.", true);
+                yield return new WaitForSeconds(5);
+                ResultTextBox.Clear();
+            }
+            if (request.text.Trim() == "TAKEN")
+            {
+                ResultTextBox.SetResultMessage("This username is already taken!");
+                yield return new WaitForSeconds(5);
+                ResultTextBox.Clear();
+            }
+        }
+        request.Dispose();
+    }
+}
---
+++ Assets/Scrypts/LoginForm/urlHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class UrlHelper : MonoBehaviour {
+   private const string URL = "https://credentials-handler-vorby.c9users.io/Login.php?Act={0}&User={1}&Pass={2}";
+    
+    public static string ConstructUrl(string action,string login, string password)
+    {
+        if (!InputIsOkay(action, login, password)) throw new Exception("The input was not correct!");
+        return string.Format(URL, action,login,password);
+    }
+
+    private static bool InputIsOkay(string action, string login, string password)
+    {
+        Debug.Log(action + login + password);
+        return (!string.IsNullOrEmpty(action) && !string.IsNullOrEmpty(login) && !string.IsNullOrEmpty(password) && login.Length < 51 && password.Length < 51);
+    }
+}
