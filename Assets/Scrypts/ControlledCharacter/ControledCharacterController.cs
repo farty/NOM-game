27852a93f1c7ebbb9b3eedcf17ec731b5fc9daa7
VorbyT <artem.slepentsev@gmail.com>
2017-11-02 19:46:33 +0300

    Refactored movement and rotation to 'Controlable' classes also player name passed through PlayerPrefs. Player prefab added

---
+++ Assets/Scrypts/ControlledCharacter/ControledCharacterController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scrypts.ControlledCharacter
+{
+    class ControledCharacterController : MonoBehaviour, IControlable
+    {
+
+        public void Movement(float speed, Vector3 location, Transform transform)
+        {
+            transform.Translate(location * speed * Time.deltaTime, Space.World);
+        }
+
+        public void Rotate(float speed, Vector3 orientation, Quaternion quaternion)
+        {
+            quaternion = Quaternion.Slerp(quaternion, Quaternion.LookRotation(orientation.normalized), speed);
+        }
+    }
+}
---
+++ Assets/Scrypts/ControlledCharacter/IControlable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scrypts.ControlledCharacter
+{
+    public interface IControlable
+    {
+        void Movement(float speed, Vector3 location, Transform transform);
+        void Rotate(float speead, Vector3 orientation, Quaternion quaternion);
+    }
+}
