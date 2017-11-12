bcc04f1c5fb8598a6ee8d23a12de255d95532b13
farty <Farty1991@gmail.com>
2017-11-12 23:16:17 +0300

    added gameMenu

---
+++ Assets/Scrypts/Menu/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+	public void GameArena()
+	{
+		Application.LoadLevel (1);
+	}
+
+	public void GameExit()
+	{
+		Application.Quit ();
+	}
+}
